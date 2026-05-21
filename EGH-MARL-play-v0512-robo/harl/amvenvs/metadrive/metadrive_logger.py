44f680df35a74380dac459b9d3d8fdd67fc8e585
buaaerxuyang <2380796520@qq.com>
2026-05-21 18:42:04 +0800

    0521-delete

--- EGH-MARL-play-v0512-robo/harl/amvenvs/metadrive/metadrive_logger.py
+++
@@ -1,6 +0,0 @@
-from amb.envs.base_logger import BaseLogger
-
-
-class MetaDriveLogger(BaseLogger):
-    def get_task_name(self):
-        return self.env_args["scenario"]
--- EGH-MARL-play-v0512-robo/harl/amvenvs/voltage/var_voltage_control/voltage_barrier/l1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-
-def l1(vs, v_ref=1.0):
-    def _l1(v):
-        return np.abs( v - v_ref )
-    return np.array([_l1(v) for v in vs])
